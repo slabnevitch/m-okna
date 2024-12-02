eb21d3911c9b674bc2e3dd08c1066eb9dc0c71f2
Slabnevitch <handofschpilrain@mail.ru>
2024-12-02 16:40:15 +0500

    faq spoilers

--- app/js/main.js
+++ app/js/main.js
@@ -136,7 +136,7 @@ console.log(Quiz)
 // require('~/app/libs-vanilla/accordion/vanilla-accordion.js')
 
 //- spoilers-------------------------
-// require('~/app/libs-vanilla/spoilers/spoilers.js')
+require('~/app/libs-vanilla/spoilers/spoilers.js')
 	
 //- tabs--------------------------
 // require('~/app/libs-vanilla/tabs/tabs-pure.js')
